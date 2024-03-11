c96230bf2f2a52b23ab4a7878aecc28079ac2f78
mzahr <amirreza.zahraei1@rbe.sk.ca>
2024-03-11 14:44:26 -0600

    readme.md is written and a setup.py is provided for quick install

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # CSP_with_RB_example
-Constraint satisfaction problem using random binary generator model 
+Constraint satisfaction problem using random binary generator model
 
+## Goal
+The goal of this program was to make a simple constraint satisfaction
+problem solver using various methods and options plus implementing the RB
+model for random generating ofCSP. We mainly use the Backtracking search algorithm with option of using optimizations full look ahead and
+forward-checking. The model is implemented to purely use binary constraints.
 
 ## Compilation and Execution
-There is no specific compiling command. Just run the main.py
-with a python interpreter in the same folder as it is.
+To run the program please run the main.py in the same folder as 
+it is with other packages.
 
-you probably can run 
-
+in linux
 ```cmd
 python3 main.py
 ```
+
+in windows
+```cmd
+python main.py
+```
+
+If you want to use the CSPInst package for your internal
+use you can install it with the "setup.py" provided.
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import setup
+
+
+setup(
+    name='CSPInst',
+    version='1.0',
+    description='A scientific simple package for solving constraint satisfaction problems.',
+    author='Mohsen Zahraei',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Natural Language :: English',
+        'Topic :: Software Development :: AI TOOLS',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.11',
+    ],
+    keywords="Constraint Satisfaction Problem, CSP, RB model, Random CSP, AI tools, AI",
+    author_email='mzx776@uregina.ca',
+    url='https://github.com/AmirrezaZahraei1387/CSP_with_RB_example',
+    packages=setuptools.find_packages(),
+)
+
